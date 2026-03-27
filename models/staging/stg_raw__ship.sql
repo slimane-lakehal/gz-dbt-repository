f2275e32d7b5768dc7fe4288bb1c70ab63b8da9b
Slimane Lakehal <lakehalslimane@gmail.com>
2026-03-27 19:19:33 +0000

    added models

---
+++ models/staging/stg_raw__ship.sql
@@ -0,0 +1,23 @@
+{{config(materialized='view')}}
+
+with 
+
+source as (
+
+    select * from {{ source('raw', 'ship') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        logcost,
+        cast(ship_cost as float64) as ship_cost
+
+    from source
+
+)
+
+select * from {{ source('raw', 'ship') }}
